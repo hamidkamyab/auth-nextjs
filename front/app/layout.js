43e7f5a4bf2adef69465136efa14189b30fe4736
hamidkamyab <mr_kamyab@yahoo.com>
2025-03-13 22:02:53 +0330

    create repo

---
+++ front/app/layout.js
@@ -0,0 +1,34 @@
+import "./globals.css";
+
+export const metadata = {
+  title: "Auth Service",
+  description: "Auth Service Api",
+};
+
+export default function RootLayout({ children }) {
+  const items = [
+    {
+      key: 1,
+      label: "<span className='hover:bg-blue-300'>Home</span>",
+    },
+    {
+      key: 2,
+      label: "Register",
+    },
+    {
+      key: 3,
+      label: "login",
+    },
+    {
+      key: 4,
+      label: "About",
+    },
+  ];
+  return (
+    <html lang="en">
+      <body>
+        <div>{children}</div>
+      </body>
+    </html>
+  );
+}
